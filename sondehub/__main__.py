134a3ba7c4731b454f6bc1a7e2764bb95517501c
Michaela <git@michaela.lgbt>
2021-02-02 10:47:04 +1000

    fixes for python 3.6

--- sondehub/__main__.py
+++ sondehub/__main__.py
@@ -7,11 +7,12 @@ def on_message(message):
 
 def main():
     parser = argparse.ArgumentParser(description='Sondehub CLI')
-    parser.add_argument('--serial', dest="sondes", default=["#"],nargs="*", help="Filter to sonde serial", type=str, action="extend")
+    parser.add_argument('--serial', dest="sondes", default=["#"],nargs="*", help="Filter to sonde serial", type=str, action="append")
     args = parser.parse_args()
     if len(args.sondes) > 1: # we need to drop the default value if the user specifies sepcific sondes
         args.sondes = args.sondes[1:]
-    test = sondehub.Stream(on_message=on_message, sondes=args.sondes)
+    sondes = [item for sublist in args.sondes for item in sublist]
+    test = sondehub.Stream(on_message=on_message, sondes=sondes)
     while 1:
         pass
 
